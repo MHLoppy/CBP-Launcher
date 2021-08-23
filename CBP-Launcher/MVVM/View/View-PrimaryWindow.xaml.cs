b02c34e49337adbd8be2d613cfaf72d45634550a
MHLoppy <MHLoppy@users.noreply.github.com>
2021-08-23 18:09:14 +1000

    Downgrade to .NET Framework 4.7.2; Add skeleton of MVVM files

---
+++ CBP-Launcher/MVVM/View/View-PrimaryWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CBPLauncher.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for View_PrimaryWindow.xaml
+    /// </summary>
+    public partial class View_PrimaryWindow : Window
+    {
+        public View_PrimaryWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
