c60bc59f6837c9a5f8f51715f7670328e08c3a2c
MHLoppy <MHLoppy@users.noreply.github.com>
2021-10-14 01:29:40 +1100

    Remove CBP Launcher from CBP Setup (NON GUI)

--- CBP-Setup/Program.cs
+++ CBP-Setup/Program.cs
@@ -8,7 +8,7 @@ using System.Threading; // Task.Delay would be nicer but seems harder to use for
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-using CBPLauncher; // in case I need to move dirs
+//using CBPLauncher; // in case I need to move dirs
 using CBPSetup.Language;
 
 namespace CBPSetup
