16c70aa6608f3ac73327963e2c57a28acbf2506a
MHLoppy <MHLoppy@users.noreply.github.com>
2022-02-19 16:51:52 +1100

    Remove MPL license headers

--- CBP-Launcher/MainWindow.xaml.cs
+++ CBP-Launcher/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
-// sometimes comments will refer to a "reference [program]" which refers to https://github.com/tom-weiland/csharp-game-launcher
+﻿// sometimes comments will refer to a "reference [program]" which refers to https://github.com/tom-weiland/csharp-game-launcher
 
 using Microsoft.Win32;
 using System;
