b2dc407cabe45641a1344da3a0d797efb683ff36
MHLoppy <MHLoppy@users.noreply.github.com>
2022-02-23 18:54:10 +1100

    Add Challenger to version array

--- CBP-Launcher/Logic/MainCode.cs
+++ CBP-Launcher/Logic/MainCode.cs
@@ -4620,11 +4620,13 @@ namespace CBPLauncher.Logic
     public class VersionArray //this seems like an inelegant way to implement the string array? but I wasn't sure where else to put it (and have it work)
     {
         //cheeky bit of extra changes to convert the numerical/int based X.Y.Z into the versioning I already used before this launcher
-        public static string[] versionStart = new string[11] { "not installed", "Pre-Alpha ", "Alpha ", "Beta ", "Release Candidate ", "1.", "2.", "3.", "4.", "5.", "6." }; // I am a fucking god figuring out how to properly use these arrays based on 10 fragments of 5% knowledge each
-        public static string[] versionMiddle = new string[16] { "", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15" }; // I don't even know what "static" means in this context, I just know what I need to use it
+        public static string[] versionStart = new string[12] { "not installed", "Pre-Alpha ", "Alpha ", "Beta ", "Release Candidate "
+                                                               , "1.", "2.", "3.", "4.", "5.", "6." // 1.x is 5 in the array (6th value including 0)
+                                                               , "Challenger "}; //Challenger is 11 (12th value including 0)
+        public static string[] versionMiddle = new string[16] { "", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15" };
         public static string[] versionEnd = new string[17] { "", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p" }; //e.g. can optionally just skip the subminor by intentionally using [0]
         public static string[] versionHotfix = new string[21] { "", " (hotfix 1)", " (hotfix 2)", " (hotfix 3)", " (hotfix 4)", " (hotfix 5)", " (hotfix 6)", " (hotfix 7)", " (hotfix 8)", " (hotfix 9)" // 0-9 respectively
-                                                              , " (special)" // 10
-                                                              , " (PR1)", " (PR2)", " (PR3)", " (PR4)", " (PR5)", " (PR6)", " (PR7)", " (PR8)", " (PR9)", " (PR10+)" }; // 11-19 respectively, then 20 for 10+ because oh god why are there so many PRs
+                                                                , " (special)" // 10
+                                                                , " (PR1)", " (PR2)", " (PR3)", " (PR4)", " (PR5)", " (PR6)", " (PR7)", " (PR8)", " (PR9)", " (PR10+)" }; // 11-19 respectively, then 20 for 10+ because oh god why are there so many PRs
     }
 }
